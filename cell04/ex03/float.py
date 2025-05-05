bbfcc10174c391d63d0e842a6211331530e25f44
Sukphiachaya Khanereo <you@example.com>
2025-05-05 22:17:21 +0700

    cell04 ex03

---
+++ cell04/ex03/float.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+num = float(input("Give me a number: "))
+num2 = int(num)
+if num - num2 != 0:
+    print("This number is an decimal.")
+else :
+    print("This number is an integer.")
