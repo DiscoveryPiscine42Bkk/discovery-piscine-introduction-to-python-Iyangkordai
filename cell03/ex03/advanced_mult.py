7d7b32dbf6cff175073e432b155592e9f26202fe
Sukphiachaya Khanereo <you@example.com>
2025-05-05 21:52:41 +0700

    cell03 ex03

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -3,7 +3,8 @@
 for i in range(0,11):
     print(f"Table de {i}: ",  end="")
     for y in range(0,11):
-        print(f"{ i* y}",sep=" ")
-        y+=1
-    i += 1
-        
+        if y != 10:
+            print(f"{i*y}" , end=" ")
+        else:
+            print(i*y)
+         
