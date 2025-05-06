434b8521e89ada2875b3f06613866b3c26a3a8a3
Sukphiachaya Khanereo <you@example.com>
2025-05-06 22:45:08 +0700

    cell07 ex01

---
+++ cell07/ex01/family_affairs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+dupout_family = {
+    "florian": "red",
+    "marie": "blond",
+    "virginie": "brunette",
+    "david": "red",
+    "franck": "red"
+}
+print(dupout_family.items())
+ans = []
+class red:
+    def find_the_redheads(self,dict):
+        filter_dict = {k: v for k, v in dict.items() if v == 'red'}
+        name = list(filter_dict.keys())
+        return name
+family = red()
+print(family.find_the_redheads(dupout_family))
