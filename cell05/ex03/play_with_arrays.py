7078aa194dcda7a246fe58551847b3d9a0fbcfee
Sukphichaya Khanereo <sukpkhan@z1t4c3.42bangkok.com>
2025-05-06 10:57:17 +0700

    cell05 ex03

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -2,9 +2,13 @@
 
 old = [2,8,9,48,8,22,-12,2]
 new = []
+new_set = {}
 for i in old:
     now = i+2
     if now >= 5:
         new.append(i+2)
+    else:
+        continue
+new_set = set(new)
 print(f"Original array : {old}")
-print(f"New array: {new}")
+print(f"New array: {new_set}")
