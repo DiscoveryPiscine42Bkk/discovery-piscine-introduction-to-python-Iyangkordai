4a68e07c7cea470bd0eb8dd7160d6bcc1bdd1f0c
Sukphichaya Khanereo <sukpkhan@z1t4c3.42bangkok.com>
2025-05-06 15:49:05 +0700

    doing cell07 ex00

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -9,9 +9,7 @@ if check < 2:
     print("none")
 else:
     for i in range(1,check):
-        now = len(arg[i])
         if re.search("ism$",arg[i]):
             continue
         else:
             print(f"{arg[i]}ism")
-
---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+ans = []
+person = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+class name:
+    def array_of_name(self,name):
+        print(name)
+
+obj = name()
+obj.array_of_name(len(person))
