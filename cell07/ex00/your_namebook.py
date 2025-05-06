80c77bd0abfb1a2685db18051dd94cb071a5c36d
Sukphiachaya Khanereo <you@example.com>
2025-05-06 21:52:34 +0700

    finish cell07 ex00

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -7,8 +7,13 @@ person = {
     "fifi": "brindacier"
 }
 class name:
-    def array_of_name(self,name):
-        print(name)
-
+    def array_of_name(self,name,lastname):
+        name = str(name).capitalize()
+        lastname = str(lastname).capitalize()
+        ans.append(f"{name} {lastname}")
+first_n =list(person.keys())
+last_n = list(person.values())
 obj = name()
-obj.array_of_name(len(person))
+for i in range(0,len(person)):
+    obj.array_of_name(first_n[i], last_n[i])
+print(ans)
