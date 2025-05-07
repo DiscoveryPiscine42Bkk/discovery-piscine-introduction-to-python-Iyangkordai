2746c7fd67f33af0dabf3437686ab043e2698bb9
Sukphichaya Khanereo <sukpkhan@z2t10c2.42bangkok.com>
2025-05-07 12:21:13 +0700

    fix cell04 ex02

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -4,9 +4,9 @@ first_num = int(input("Give me the first number: "))
 second_num = int(input("Give me the second number: "))
 print("Thank you")
 opt = ["+","-","/","*"]
-if second_num == 0:
-    ans = [first_num + second_num , first_num - second_num , "Error", first_num * second_num]
-else:
+try:
     ans = [first_num + second_num , first_num - second_num , int(first_num / second_num) , first_num * second_num]
-for i in range(0,4):
-    print(f"{first_num} {opt[i]} {second_num} = {ans[i]}")
+    for i in range(0,4):
+        print(f"{first_num} {opt[i]} {second_num} = {ans[i]}")
+except:
+    print("Error!")
